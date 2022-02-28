c9ac48aa3469d2d8f7270a60fa73a668bf2a49b9
KRLEESEUNGJAE <karmaup0711@gmail.com>
2022-02-28 22:59:01 +0900

    Add PageComponent function

--- 12-motion-project/src/components/page/item/video.ts
+++ 12-motion-project/src/components/page/item/video.ts
@@ -7,12 +7,31 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
           </section>`);
 
     const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
-    console.log(url);
+    // console.log(url);
 
-    iframe.src = 'https://www.youtube.com/embed/-ObdvMkCKws'; // url -> videoId -> embed
+    // iframe.src = 'https://www.youtube.com/embed/-ObdvMkCKws'; //Todo: url -> videoId -> embed
+    iframe.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement;
     titleElement.textContent = title;
+  }
+
+  //* input
+  // https://www.youtube.com/watch?v=-ObdvMkCKws
+  // https://youtu.be/-ObdvMkCKws
+  //* output
+  // https://www.youtube.com/embed/-ObdvMkCKws //? -> 이 형식으로 만들어야 함!
+  // 정규표현식 Regex를 이용하여 추출
+  private convertToEmbeddedURL(url: string): string {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    console.log(match);
+
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
   }
 }
 
